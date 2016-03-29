5a92a53f797765d3bfb8d838d1dd4c1c68477524
Sam Giles <sam.e.giles@gmail.com>
2016-03-29 03:53:09 +0100

    Full suite of readers

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
+mod reader;
+
+use reader::*;
+
 use std::io;
 use std::io::ErrorKind;
 use std::mem;
+
+
 
 pub struct FbxLoader {
    current_offset: usize
@@ -18,35 +24,24 @@ fn _verify_fbx_header(reader: &mut io::Read) -> io::Result<usize> {
     }
 }
 
-fn _read_u32(reader: &mut io::Read) -> io::Result<u32> {
-    let mut version_buffer = [0 as u8; 4];
-    try!(reader.read_exact(&mut version_buffer));
-
-    let value: u32 = unsafe { mem::transmute(version_buffer) };
-    Ok(value)
+pub enum PropertyType {
+    I16(i16),
+    Bool(bool),
+    I32(i32),
+    I64(i64),
+    F32(f32),
+    F64(f64),
+    BinaryData(Vec<u8>),
+    StringData(String),
+    ArrayF32(Vec<f32>),
+    ArrayI32(Vec<i32>),
+    ArrayF64(Vec<f64>),
+    ArrayI64(Vec<i64>),
+    ArrayBool(Vec<u8>),
+    ArrayU8(Vec<u8>)
 }
 
-fn _read_u8(reader: &mut io::Read) -> io::Result<u8> {
-    let mut version_buffer = [0 as u8; 1];
-    try!(reader.read_exact(&mut version_buffer));
 
-    Ok(version_buffer[0])
-}
-
-fn _read_string(reader: &mut io::Read) -> io::Result<String> {
-    let mut string = String::new();
-
-    let mut len = try!(_read_u8(reader));
-
-    while len > 0 {
-        len -= 1;
-        let mut buffer = [0 as u8; 1];
-        try!(reader.read_exact(&mut buffer));
-        string.push(buffer[0] as char)
-    }
-
-    Ok(string)
-}
 
 impl FbxLoader {
     pub fn new() -> Self {
@@ -59,7 +54,7 @@ impl FbxLoader {
     }
 
     fn read_fbx_version(&mut self, reader: &mut io::Read) -> io::Result<u32> {
-        let version: u32 = try!(_read_u32(reader));
+        let version: u32 = try!(read_u32(reader));
 
         self.current_offset += 4;
         Ok(version)
@@ -67,7 +62,7 @@ impl FbxLoader {
 
     fn read_element(&mut self, reader: &mut io::Read) -> io::Result<Option<usize>> {
         // [0] = offset at which this block ends - u32
-        let end_offset = try!(_read_u32(reader));
+        let end_offset = try!(read_u32(reader));
 
         if end_offset == 0 {
             return Ok(None)
@@ -75,8 +70,65 @@ impl FbxLoader {
 
         // [1] = number of props in the scope -  u32
         // [2] = length of the property list - u32
-        let property_count  = try!(_read_u32(reader));
-        let property_length = try!(_read_u32(reader));
+        let property_count  = try!(read_u32(reader)) as usize;
+        let property_length = try!(read_u32(reader)) as usize;
+
+        let element_id = try!(read_string(reader));
+        let element_property_types: Vec<u8> = Vec::with_capacity(property_count);
+        let element_property_data:  Vec<Option<()>> = Vec::with_capacity(property_count);
+
+        for index in 0..property_count {
+            let data_type = try!(read_u8(reader));
+
+            // assumes little endian
+            let _ = match data_type {
+                0x59 /* i16 */ => {
+                    PropertyType::I16(try!(read_i16(reader)))
+                },
+                0x43 /* 1 byte bool */ => {
+                    PropertyType::Bool(try!(read_u8(reader)) != 0)
+                },
+                0x49 /* i32 */ => {
+                    PropertyType::I32(try!(read_i32(reader)))
+                },
+                0x46 /* f32 */ => {
+                    PropertyType::F32(try!(read_f32(reader)))
+                },
+                0x44 /* f64 */ => {
+                    PropertyType::F64(try!(read_f64(reader)))
+                },
+                0x4c /* i64 */ => {
+                    PropertyType::I64(try!(read_i64(reader)))
+                },
+                0x52 /* binary data */ => {
+                    PropertyType::BinaryData(try!(read_u8_array(reader)))
+                },
+                0x53 /* string data */ => {
+                    PropertyType::StringData(try!(read_string(reader)))
+                },
+                0x66 /* array(f32) */ => {
+                    PropertyType::ArrayF32(try!(read_f32_array(reader)))
+                },
+                0x69 /* array(i32) */ => {
+                    PropertyType::ArrayI32(try!(read_i32_array(reader)))
+                },
+                0x64 /* array(f64) */ => {
+                    PropertyType::ArrayF64(try!(read_f64_array(reader)))
+                },
+                0x6c /* array(i64) */ => {
+                    PropertyType::ArrayI64(try!(read_i64_array(reader)))
+                },
+                0x62 /* array(bool) */ => {
+                    PropertyType::ArrayBool(try!(read_u8_array(reader)))
+                },
+                0x63 /* array(u8) */ => {
+                    PropertyType::ArrayU8(try!(read_u8_array(reader)))
+                },
+                _ => {
+                    panic!("unknown property type - invalid FBX file")
+                }
+            };
+        }
 
 
 
@@ -93,17 +145,10 @@ impl FbxLoader {
 #[cfg(test)]
 mod tests {
     use std::io;
-    use super::{ _read_string, _verify_fbx_header };
+    use reader::read_string;
     use super::*;
+    use super::_verify_fbx_header;
 
-    #[test]
-    fn test_read_string() {
-        // The length of the string is indicated by a byte value before it
-        let string = "\x08A string".as_bytes();
-
-        let mut reader = io::Cursor::new(string);
-        assert_eq!(_read_string(&mut reader).unwrap(), "A string");
-    }
 
     #[test]
     fn test_header_ok() {
---
+++ src/reader.rs
@@ -0,0 +1,174 @@
+use std::io;
+use std::io::ErrorKind;
+use std::mem;
+
+pub fn read_u8(reader: &mut io::Read) -> io::Result<u8> {
+    let mut buffer = [0 as u8; 1];
+    try!(reader.read_exact(&mut buffer));
+
+    Ok(buffer[0])
+}
+
+pub fn read_u32(reader: &mut io::Read) -> io::Result<u32> {
+    let mut buffer = [0 as u8; 4];
+    try!(reader.read_exact(&mut buffer));
+
+    let value: u32 = unsafe { mem::transmute(buffer) };
+    Ok(value)
+}
+
+pub fn read_i16(reader: &mut io::Read) -> io::Result<i16> {
+    let mut buffer = [0 as u8; 2];
+    try!(reader.read_exact(&mut buffer));
+
+    let value: i16 = unsafe { mem::transmute(buffer) };
+    Ok(value)
+}
+
+pub fn read_i32(reader: &mut io::Read) -> io::Result<i32> {
+    let mut buffer = [0 as u8; 4];
+    try!(reader.read_exact(&mut buffer));
+
+    let value: i32 = unsafe { mem::transmute(buffer) };
+    Ok(value)
+}
+
+pub fn read_i64(reader: &mut io::Read) -> io::Result<i64> {
+    let mut buffer = [0 as u8; 8];
+    try!(reader.read_exact(&mut buffer));
+
+    let value: i64 = unsafe { mem::transmute(buffer) };
+    Ok(value)
+}
+
+pub fn read_f32(reader: &mut io::Read) -> io::Result<f32> {
+    let mut buffer = [0 as u8; 4];
+    try!(reader.read_exact(&mut buffer));
+
+    let value: f32 = unsafe { mem::transmute(buffer) };
+    Ok(value)
+}
+
+pub fn read_f64(reader: &mut io::Read) -> io::Result<f64> {
+    let mut buffer = [0 as u8; 8];
+    try!(reader.read_exact(&mut buffer));
+
+    let value: f64 = unsafe { mem::transmute(buffer) };
+    Ok(value)
+}
+
+macro_rules! read_array {
+    ($reader:expr, $len:expr, $t:ty, $buffer:expr) => {
+        {
+        let mut chunks: Vec<$t> = Vec::new();
+        let mut len = $len;
+
+        while len > 0 {
+            let mut buffer = $buffer;
+            try!($reader.read_exact(&mut buffer));
+
+            chunks.push(unsafe { mem::transmute(buffer) });
+
+            len = len - buffer.len();
+        }
+
+        Ok(chunks)
+        }
+    };
+}
+
+pub fn read_u8_array(reader: &mut io::Read) -> io::Result<Vec<u8>> {
+    let (len, encoded, compressed_length) = try!(read_array_header(reader));
+    if encoded {
+        unimplemented!()
+    } else {
+        read_array!(reader, len, u8, [0 as u8; 1])
+    }
+}
+
+pub fn read_u32_array(reader: &mut io::Read) -> io::Result<Vec<u32>> {
+    let (len, encoded, compressed_length) = try!(read_array_header(reader));
+    if encoded {
+        unimplemented!()
+    } else {
+        read_array!(reader, len, u32, [0 as u8; 4])
+    }
+}
+
+pub fn read_i16_array(reader: &mut io::Read) -> io::Result<Vec<i16>> {
+    let (len, encoded, compressed_length) = try!(read_array_header(reader));
+    if encoded {
+        unimplemented!()
+    } else {
+        read_array!(reader, len, i16, [0 as u8; 2])
+    }
+}
+
+pub fn read_i32_array(reader: &mut io::Read) -> io::Result<Vec<i32>> {
+    let (len, encoded, compressed_length) = try!(read_array_header(reader));
+    if encoded {
+        unimplemented!()
+    } else {
+        read_array!(reader, len, i32, [0 as u8; 4])
+    }
+}
+
+pub fn read_i64_array(reader: &mut io::Read) -> io::Result<Vec<i64>> {
+    let (len, encoded, compressed_length) = try!(read_array_header(reader));
+    if encoded {
+        unimplemented!()
+    } else {
+        read_array!(reader, len, i64, [0 as u8; 8])
+    }
+}
+
+pub fn read_f32_array(reader: &mut io::Read) -> io::Result<Vec<f32>> {
+    let (len, encoded, compressed_length) = try!(read_array_header(reader));
+    if encoded {
+        unimplemented!()
+    } else {
+        read_array!(reader, len, f32, [0 as u8; 4])
+    }
+}
+
+pub fn read_f64_array(reader: &mut io::Read) -> io::Result<Vec<f64>> {
+    let (len, encoded, compressed_length) = try!(read_array_header(reader));
+    if encoded {
+        unimplemented!()
+    } else {
+        read_array!(reader, len, f64, [0 as u8; 8])
+    }
+}
+
+pub fn read_array_header(reader: &mut io::Read) -> io::Result<(usize, bool, usize)> { Ok(
+        (try!(read_u32(reader)) as usize, // Length
+         try!(read_u32(reader)) != 0,     // Encoding bool
+         try!(read_u32(reader)) as usize  // compressed length
+        )
+    )
+}
+
+
+
+pub fn read_string(reader: &mut io::Read) -> io::Result<String> {
+    let len = try!(read_u8(reader)) as usize;
+
+    let bytes: io::Result<Vec<u8>> = read_array!(reader, len, u8, [0 as u8; 1]);
+    let string = unsafe { String::from_utf8_unchecked(try!(bytes)) };
+    Ok(string)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+    use super::*;
+
+    #[test]
+    fn test_read_string() {
+        // The length of the string is indicated by a byte value before it
+        let string = "\x08A string".as_bytes();
+
+        let mut reader = io::Cursor::new(string);
+        assert_eq!(read_string(&mut reader).unwrap(), "A string");
+    }
+}
